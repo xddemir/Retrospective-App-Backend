9519b37aa9b93f9df25e301020646a149386d633
Dogukan Demir <dogukan.demir991@gmail.com>
2023-05-23 16:41:44 +0300

    Added outlook email service.

--- controllers/invite.ts
+++ controllers/invite.ts
@@ -1,7 +1,8 @@
-import { error } from "console";
+import { error, info } from "console";
 import { validationResult } from "express-validator";
 import jwt from "jsonwebtoken";
 import { getCache } from "../services/cache";
+import { OutlookSender } from "../services/mail";
 
 export function inviteUser(req: any, res: any, next: any) {
   const errors = validationResult(error);
@@ -10,6 +11,8 @@ export function inviteUser(req: any, res: any, next: any) {
     error.statusCode = 422;
     throw error;
   }
+
+  let linkUrl: string;
 
   getCache(req.userId)
     .then((retro: any) => {
@@ -23,17 +26,34 @@ export function inviteUser(req: any, res: any, next: any) {
 
       const payload = {
         retroId: retro._id,
-        userId: req.userId
+        userId: req.userId,
       };
 
       const token = jwt.sign(payload, "demirdogukan", {
         expiresIn: "15m",
       });
+
       const link = `${process.env.BASE_URL}/addUser/${token}`;
 
-      console.log(link);
+      linkUrl = link;
 
-      res.status(200).json({ invitationUrl: link });
+      let sender: OutlookSender = new OutlookSender({
+        service: "hotmail",
+        auth: {
+          user: process.env.EMAIL_USER!,
+          pass: process.env.EMAIL_PASSWORD!,
+        },
+      });
+
+      return sender.sendMail({
+        from: process.env.EMAIL_USER!,
+        to: "",
+        subject: "Retro Invitation Link",
+        text: linkUrl,
+      });
+    })
+    .then((result: any) => {
+      res.status(200).json({ message: result, invitationUrl: linkUrl });
     })
     .catch((err) => {
       console.log("Value is null");
